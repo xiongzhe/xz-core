ca4aa312a2ed11168a94cc833d2ef91bfab2f710
熊哲 <804117743@qq.com>
2019-04-17 15:11:55 +0800

    init

--- core/src/main/java/com/xz/android/core/util/device/VibratorUtil.java
+++ core/src/main/java/com/xz/android/core/util/device/VibratorUtil.java
@@ -15,7 +15,6 @@ public class VibratorUtil {
      * 手机振动器
      */
     public static void vibrate(Context context) {
-
         Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
         vibrator.vibrate(200);
     }
